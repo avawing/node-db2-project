02e2be66e21dc5b25ebb873ffe00ea8224b09bfe
Aveline Wingfield <avelinewingfield@gmail.com>
2020-09-08 17:39:01 -0400

    All's well that exists

--- migrations/cars.js
+++ migrations/cars.js
@@ -1,23 +1,22 @@
-
-exports.up = function(knex, Promise) {
-  return knex.schema.createTable('cars', tbl=>{
-      //creates id
-      tbl.increments();
-      //VIN (number)
-      tbl.string("vin", 17).unique().notNullable();
-      //make (string)
-      tbl.string("make").notNullable();
-      //model (string)
-      tbl.string("model").notNullable();
-      //mileage (number)
-      tbl.float("mileage", 9).notNullable();
-      //transmission type - optional (string)
-      tbl.string("transmission");
-      //status of title - optional (string)
-      tbl.text("title");
-  })
+exports.up = function (knex, Promise) {
+  return knex.schema.createTable("cars", (tbl) => {
+    //creates id
+    tbl.increments();
+    //VIN (number)
+    tbl.string("vin", 17).unique().notNullable();
+    //make (string)
+    tbl.string("make").notNullable();
+    //model (string)
+    tbl.string("model").notNullable();
+    //mileage (number)
+    tbl.float("mileage", 9).notNullable();
+    //transmission type - optional (string)
+    tbl.string("transmission");
+    //status of title - optional (string)
+    tbl.text("title");
+  });
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex, Promise) {
+  return sknex.schema.dropTableIfExists("cars");
 };
--- server.js
+++ server.js
@@ -1,7 +1,54 @@
-const express = require('express')
-const server = express()
+const express = require("express");
 
+//for sprint, set up helpers and routes
+//for this, forget that nonsense
+const db = require("./knexconfig");
+const server = express();
 server.use(express.json());
 
-const PORT = process.env.PORT || 4000
-server.listen(PORT, () =>{console.log("server is running")})
+server.get("/", (req, res) => {
+  res.send("What up?");
+});
+
+server.get("/cars", (req, res) => {
+  db("cars")
+    .then((cars) => {
+      if (cars.length) {
+        res.status(200).json({ data: cars }).end();
+      } else {
+        res
+          .status(400)
+          .json({ message: "Server is wrong, or empty!", data: cars })
+          .end();
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "Server is very wrong" }).end();
+    });
+});
+server.post("/cars", (req, res) => {
+
+  db("cars").insert(req.body)
+    .then((car) => {
+      if (car.length) {
+        const id = car[0];
+        db("cars")
+          .where({ id })
+          .then((car) => {
+            res.status(201).json(car).end();
+          })
+          .catch((err) =>
+            res.status(500).json({ message: "server fucked up" }).end()
+          );
+      } else {
+        res.status(400).json({ mesage: "Bad Request" }).end();
+      }
+    })
+    .catch((err) =>
+      res.status(500).json({ message: "Server REALLY fucked up" }).end()
+    );
+});
+const PORT = process.env.PORT || 4000;
+server.listen(PORT, () => {
+  console.log("server is running");
+});
